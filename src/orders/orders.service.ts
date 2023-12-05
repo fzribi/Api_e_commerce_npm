353c80adfe3faedc505a3b6ac58ef90a597ffd7f
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-05 18:10:09 +0100

    feat: ajout import PrismaService, constructor et Providers PrismaService

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class OrdersService {
+  constructor(private readonly prisma: PrismaService) {}
+  
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
 export class CreateUserDto {}
+"user_UUID" VARCHAR(36) NOT NULL,
+"user_pseudo" VARCHAR(20) NOT NULL,
+"username" VARCHAR(30) NOT NULL,
+"user_password" VARCHAR(72) NOT NULL,
+"created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+user_pseudo
+username
+user_password
+created_at
