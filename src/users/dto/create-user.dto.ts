a3cd1afa9fd34a07eb98e3f4fb13595c78e33f04
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-06 13:45:57 +0100

    feat: ajout de pseudo, username, password, created at dans user.dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,7 @@
-export class CreateUserDto {}
-"user_UUID" VARCHAR(36) NOT NULL,
-"user_pseudo" VARCHAR(20) NOT NULL,
-"username" VARCHAR(30) NOT NULL,
-"user_password" VARCHAR(72) NOT NULL,
-"created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+export class CreateUserDto {
 
-user_pseudo
-username
-user_password
-created_at
+pseudo: string;
+username: string;
+password: string;
+created_at: Date;
+}
