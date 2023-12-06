2a59632d9697c7481385f3a3d02462b12c2829aa
writememory1337 <ordiplus58@outlook.fr>
2023-12-06 13:56:42 +0100

    added product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,6 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+    product_name: string;
+    product_description: string;
+    product_price: number;
+    product_quantity?: number;
+  }
--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto {
+    product_name?: string;
+    product_description?: string;
+    product_price?: number;
+    product_quantity?: number;
+  }
+  
